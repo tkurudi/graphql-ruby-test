3954d399ef7732deb8d4e8845709614f4402de6e
tkurudi <t.kurudi209@gmail.com>
2019-02-26 18:13:59 +1300

    added seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,4 +6,18 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-PROJECT_TITLES =  10.times.map { Faker::Internet.domin_word }
+PROJECT_TITLES =  10.times.map { Faker::Internet.domain_word }
+
+SPLIT = 0.4
+OFFSET = PROJECT_TITLES.length * SPLIT
+
+PROJECT_USERS = {
+    'testing@example.com' => PROJECT_TITLES[0, OFFSET],
+    'bob@example.com' => PROJECT_TITLES[OFFSET..-1]
+}
+
+PROJECT_USERS.each do |email, projects|
+   user = User.new(email: email, password: 'secret')
+   projects.each { |title| user.projects.build(title: title) }
+   user.save
+end
